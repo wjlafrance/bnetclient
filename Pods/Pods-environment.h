c7c508d2ab4db09bce63a733a691cf58715f79a6
William LaFrance <wjlafrance@gmail.com>
2013-12-04 00:09:47 -0600

    Add Pods directory so Xcode Server can build

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaAsyncSocket
+#define COCOAPODS_POD_AVAILABLE_CocoaAsyncSocket
+#define COCOAPODS_VERSION_MAJOR_CocoaAsyncSocket 0
+#define COCOAPODS_VERSION_MINOR_CocoaAsyncSocket 0
+#define COCOAPODS_VERSION_PATCH_CocoaAsyncSocket 1
+
+// Kiwi/ARC
+#define COCOAPODS_POD_AVAILABLE_Kiwi_ARC
+#define COCOAPODS_VERSION_MAJOR_Kiwi_ARC 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_ARC 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_ARC 3
+
+// Kiwi/NonARC
+#define COCOAPODS_POD_AVAILABLE_Kiwi_NonARC
+#define COCOAPODS_VERSION_MAJOR_Kiwi_NonARC 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_NonARC 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_NonARC 3
+
+// Kiwi/XCTest
+#define COCOAPODS_POD_AVAILABLE_Kiwi_XCTest
+#define COCOAPODS_VERSION_MAJOR_Kiwi_XCTest 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_XCTest 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_XCTest 3
+
+// NSLogger
+#define COCOAPODS_POD_AVAILABLE_NSLogger
+#define COCOAPODS_VERSION_MAJOR_NSLogger 1
+#define COCOAPODS_VERSION_MINOR_NSLogger 1
+#define COCOAPODS_VERSION_PATCH_NSLogger 0
+
